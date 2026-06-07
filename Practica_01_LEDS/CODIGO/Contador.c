9b1c9cdbae36d44c9ce61aaf4e7c6e1c3e571396
cristiancristan-ops <cristian.cristan@udem.edu>
2026-06-06 23:46:00 -0600

    Create Contador.c

---
+++ Practica_01_LEDS/CODIGO/Contador.c
@@ -0,0 +1,36 @@
+#include <xc.h>         // Biblioteca principal del compilador XC8
+
+//=============================================================================
+// CONFIGURACI N DE BITS DE CONFIGURACI N (FUSES)
+//=============================================================================
+
+// Selecci n de oscilador (usar XT si est s usando un cristal de 4 MHz)
+#pragma config FOSC = HS        // Oscillator Selection bits (XT oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config BOREN = ON       // Brown-out Reset Enable bit (enabled)
+#pragma config LVP = OFF        // Low-Voltage Programming Enable bit (disabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection (disabled)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable (disabled)
+#pragma config CP = OFF         // Flash Program Memory Code Protection (disabled)
+
+//=============================================================================
+// DEFINICIONES
+//=============================================================================
+
+#define _XTAL_FREQ 8000000      // Frecuencia del oscilador (para __delay_ms y __delay_us)
+
+void main(void){
+    TRISD = 0b00000000;
+    PORTD = 0b00000000;
+    unsigned char contador = 0;
+
+    while(1){
+        PORTD = contador;
+        __delay_ms(150);
+        contador++;
+
+        if (contador > 63)
+            contador = 0;
+    }
+}
